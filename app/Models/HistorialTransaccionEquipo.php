1c1039eb311d4afc0abf3fc48b5d29ce145043c2
irvinparedessv <irvin.paredes.sv@gmail.com>
2025-04-22 17:51:21 -0600

    first commit

---
+++ app/Models/HistorialTransaccionEquipo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HistorialTransaccionEquipo extends Model
+{
+    /** @use HasFactory<\Database\Factories\HistorialTransaccionEquipoFactory> */
+    use HasFactory;
+}
---
+++ app/Models/ReservaEquipo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReservaEquipo extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReservaEquipoFactory> */
+    use HasFactory;
+}
