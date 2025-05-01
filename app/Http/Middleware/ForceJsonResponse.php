102fb7235f080136940ab2d43c410f129d799223
SaulRivera99 <saulrivera9999@gmail.com>
2025-05-01 16:11:57 -0600

    campo is_deleted en tipoEquipo

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -7,11 +7,23 @@ use Illuminate\Http\Request;
 
 class ForceJsonResponse
 {
-    public function handle(Request $request, Closure $next)
-    {
-        // Asegura que siempre se espere respuesta en formato JSON
-        $request->headers->set('Accept', 'application/json');
+    // public function handle(Request $request, Closure $next)
+    // {
+    //     // Asegura que siempre se espere respuesta en formato JSON
+    //     $request->headers->set('Accept', 'application/json');
         
-        return $next($request);
+    //     return $next($request);
+    // }
+
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        // Solo forzar JSON si la respuesta es del tipo esperado
+        if ($request->wantsJson() && $response instanceof JsonResponse) {
+            return $response;
+        }
+
+        return $response;
     }
 }
