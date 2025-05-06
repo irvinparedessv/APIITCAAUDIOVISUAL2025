f53aa0a0377d3233f21e4069512eea04f92a4d8d
SaulRivera99 <saulrivera9999@gmail.com>
2025-05-05 19:21:47 -0600

    disenio de correo

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -1,14 +1,49 @@
 @component('mail::message')
-# Hola {{ $user->name }}
+{{-- Estilos inline para mejor compatibilidad con clientes de correo --}}
+<style>
+    h1 {
+        font-family: 'Georgia', serif;
+        color: rgb(139, 0, 0); /* Color principal */
+    }
+    p {
+        font-family: 'Helvetica', 'Arial', sans-serif;
+        color: #333;
+        font-size: 16px;
+        line-height: 1.6;
+    }
+    .footer {
+        margin-top: 30px;
+        font-size: 13px;
+        color: #999;
+    }
+    .custom-button {
+        background-color: rgb(139, 0, 0); /* Color principal */
+        color: #fff !important;
+        padding: 12px 20px;
+        border-radius: 4px;
+        text-decoration: none;
+        font-weight: bold;
+        display: inline-block;
+        text-align: center;
+        margin: 20px 0;
+    }
+</style>
 
-Has solicitado restablecer tu contraseña. Haz clic en el botón de abajo:
+# Estimado/a {{ $user->name }},
 
-@component('mail::button', ['url' => $url])
-Restablecer contraseña
-@endcomponent
+Hemos recibido una solicitud para restablecer la contraseña de tu cuenta. Para continuar con el proceso, haz clic en el siguiente botón:
 
-Si no hiciste esta solicitud, puedes ignorar este correo.
+<a href="{{ $url }}" class="custom-button">Restablecer Contraseña</a>
 
-Gracias,<br>
-{{ config('app.name') }}
+Si tú no realizaste esta solicitud, puedes ignorar este mensaje con total seguridad. Tu cuenta seguirá protegida.
+
+Gracias por tu atención,<br>
+**{{ config('app.name') }}**
+
+@slot('subcopy')
+<span class="footer">
+Este mensaje ha sido generado automáticamente. Por favor, no respondas a este correo.
+</span>
+@endslot
+
 @endcomponent
