444c4d6e8f20d4115eca4d75f03848bed8ab354f
LeonardoMdez <leonardomendez1516@gmail.com>
2025-05-18 18:33:07 -0600

    cambio disenio de correo general

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,9 @@
 @props(['url'])
 <tr>
-<td class="header">
-    <a href="{{ $url }}" style="display: inline-block;">
-        <img src="{{ asset('storage/logo.png') }}" class="logo" alt="ITCA-FEPADE">
-    </a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('storage/logo.png') }}" class="logo" alt="ITCA-FEPADE">
+        </a>
+    </td>
 </tr>
+
